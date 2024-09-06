404312c2c13ebcdd81a86187b8a98ac74e95f876
mrjrask <96755551+mrjrask@users.noreply.github.com>
2024-09-06 18:18:24 -0500

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,7 +1,9 @@
 const NodeHelper = require("node_helper");
 const request = require("request");
+const moment = require("moment");
 
 module.exports = NodeHelper.create({
+    finalScores: [],
     start: function() {
         console.log("MMM-LiveScores helper started...");
     },
@@ -13,6 +15,16 @@ module.exports = NodeHelper.create({
     },
 
     getSportsScores: function(config) {
+        const currentTime = moment();
+        const targetTime = moment().hour(9).minute(0);
+
+        // If before 9am, return the stored final scores
+        if (currentTime.isBefore(targetTime)) {
+            this.sendSocketNotification("SCORES_RESULT", this.finalScores);
+            return;
+        }
+
+        this.finalScores = []; // Clear final scores after 9am
         const self = this;
         const leagueEndpoints = {
             MLB: "http://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard",
@@ -31,6 +43,13 @@ module.exports = NodeHelper.create({
                     const games = self.parseScores(data);
                     scores = scores.concat(games);
 
+                    // If the game is final, add it to the finalScores array
+                    games.forEach(game => {
+                        if (game.status.toLowerCase() === "final") {
+                            self.finalScores.push(game);
+                        }
+                    });
+
                     if (scores.length >= config.leagues.length) {
                         self.sendSocketNotification("SCORES_RESULT", scores);
                     }
@@ -48,6 +67,8 @@ module.exports = NodeHelper.create({
                 awayTeam: game.competitions[0].competitors[1].team.shortDisplayName,
                 homeScore: game.competitions[0].competitors[0].score,
                 awayScore: game.competitions[0].competitors[1].score,
+                homeLogo: game.competitions[0].competitors[0].team.logo,
+                awayLogo: game.competitions[0].competitors[1].team.logo,
                 status: game.status.type.description
             };
         });
